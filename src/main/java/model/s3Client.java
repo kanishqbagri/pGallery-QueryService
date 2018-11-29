e2a00374bfc87da36d2c0efc867d2e496a9eb5e4
kbagri <kbagri@tracelink.com>
2018-11-29 13:39:07 -0500

    Adding the code to fetch bucket object from the S3 client

---
+++ src/main/java/model/s3Client.java
@@ -0,0 +1,59 @@
+package model;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class s3Client {
+
+            public static String accessKey="AKIAJPGXPPELM6QALT4Q";
+            public static String secretKey="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";
+
+            public static AmazonS3 gets3Client(){
+
+                BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
+                AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                        .withRegion("us-east-2")
+                        .withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+
+                return s3Client;
+            }
+
+            public static void getBucketList(){
+            //final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+                AmazonS3 client = gets3Client();
+
+                // List of buckets
+                List<Bucket> buckets = client.listBuckets();
+                System.out.println("Your Amazon S3 buckets are:");
+                for (Bucket b : buckets)
+                {
+                System.out.println("* " + b.getName());
+                }
+
+        }
+
+        public static List<String> getObjectsFromBucket(String bucket){
+                AmazonS3 client = gets3Client();
+                ArrayList<String> bucketList= new ArrayList<>();
+            String s3BaseUrl = "https://s3.us-east-2.amazonaws.com/photo-journal/";
+                //List of Objects in a bucket
+            String prefix = "static/img_t/" + bucket;
+                List<S3ObjectSummary> s3objects = client.listObjects("photo-journal",prefix).getObjectSummaries();
+
+                System.out.println("Your Amazon Concerts objects are:");
+                for (S3ObjectSummary a : s3objects)
+                {
+                    System.out.println("* " + a.getKey());
+                    bucketList.add(s3BaseUrl + a.getKey());
+                }
+
+            return bucketList;
+        }
+}
