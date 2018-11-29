215787c8ac81d59b7a7e60aef7eb8f5f01508d66
kbagri <kbagri@tracelink.com>
2018-11-28 23:15:57 -0500

    Adding the upload end point for File upload

--- src/main/java/com/pGallery/pGalleryqueryService/ImageResource.java
+++ src/main/java/com/pGallery/pGalleryqueryService/ImageResource.java
@@ -9,6 +9,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import service.ImageService;
 import model.Image;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import javax.websocket.server.PathParam;
 import java.security.InvalidKeyException;
 import java.util.List;
@@ -24,17 +37,12 @@ public class ImageResource {
     @RequestMapping(method = RequestMethod.GET, value = "/ping")
     public @ResponseBody String ping(){
         String [] kids= {"static/img/friendship/friendship1.jpg", "static/img/friendship/friendship2.jpg","static/img/friendship/friendship3.jpg","static/img/friendship/friendship4.jpg","static/img/friendship/friendship5.jpg","static/img/friendship/friendship6.jpg","static/img/friendship/friendship7.jpg","static/img/friendship/friendship8.jpg","static/img/friendship/friendship9.jpg"};
-        return "pong1";
+        return "pong";
         //return "pong";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/getImageData/{category}", produces = {"application/json", "application/xml"})
     public @ResponseBody List<Image> getData(@PathVariable("category") String cat){
-//        String [] friendship= {"static/img_t/friendship/friendship1.jpg", "static/img_t/friendship/friendship2.jpg","static/img_t/friendship/friendship3.jpg","static/img_t/friendship/friendship4.jpg","static/img_t/friendship/friendship5.jpg","static/img_t/friendship/friendship6.jpg","static/img_t/friendship/friendship7.jpg","static/img_t/friendship/friendship8.jpg","static/img_t/friendship/friendship9.jpg","static/img_t/friendship/friendship10.jpg","static/img_t/friendship/friendship11.jpg"};
-//        String [] kids={"static/img_t/kids/kids1.jpg", "static/img_t/kids/kids2.jpg","static/img_t/kids/kids3.jpg","static/img_t/kids/kids4.jpg","static/img_t/kids/kids5.jpg","static/img_t/kids/kids6.jpg","static/img_t/kids/kids7.jpg","static/img_t/kids/kids8.jpg","static/img_t/kids/kids9.jpg","static/img_t/kids/kids10.jpg","static/img_t/kids/kids11.jpg"};
-//        String [] seasons={"static/img_t/seasons/seasons1.jpg", "static/img_t/seasons/seasons2.jpg","static/img_t/seasons/seasons3.jpg","static/img_t/seasons/seasons4.jpg","static/img_t/seasons/seasons5.jpg","static/img_t/seasons/seasons6.jpg","static/img_t/seasons/seasons7.jpg","static/img_t/seasons/seasons8.jpg","static/img_t/seasons/seasons9.jpg","static/img_t/seasons/seasons10.jpg","static/img_t/seasons/seasons11.jpg","static/img_t/seasons/seasons12.jpg","static/img_t/seasons/seasons13.jpg"};
-//        String [] concerts={"static/img_t/concerts/concert1.jpg", "static/img_t/concerts/concert2.jpg","static/img_t/concerts/concert3.jpg","static/img_t/concerts/concert4.jpg","static/img_t/concerts/concert5.jpg","static/img_t/concerts/concert6.jpg","static/img_t/concerts/concert7.jpg","static/img_t/concerts/concert8.jpg"};
-//        String [] landscapes={"static/img_t/landscape/landscape1.jpg", "static/img_t/landscape/landscape2.jpg","static/img_t/landscape/landscape3.jpg","static/img_t/landscape/landscape4.jpg","static/img_t/landscape/landscape5.jpg","static/img_t/landscape/landscape6.jpg","static/img_t/landscape/landscape7.jpg","static/img_t/landscape/landscape8.jpg","static/img_t/landscape/landscape9.jpg"};
             List<Image> imageList=null;
         try {
             imageList= imgService.getAllMessages(cat);
@@ -44,6 +52,47 @@ public class ImageResource {
 
         return imageList;
     }
+
+    @Controller
+    public class FileUploadController {
+
+        //destination folder to upload the files
+        private String UPLOAD_FOLDER = "/tmp/";
+
+        @RequestMapping("/upload")
+        public ModelAndView showUpload() {
+            return new ModelAndView("upload");
+        }
+
+        @PostMapping("/upload")
+        public ModelAndView fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+            String status="Invalid";
+            String message="Please select a valid file";
+
+            if (file.isEmpty()) {
+            status="Invalid";
+            message="Please select a valid file";
+                return new ModelAndView(status, message, "Please select a file and try again");
+            }
+
+            try {
+                // read and write the file to the selected location-
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+                Files.write(path, bytes);
+                status="Successful";
+                message="Upload Successful";
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return new ModelAndView(status, message, "File Uploaded sucessfully");
+        }
+
+    }
+
+
 
 }
 
--- src/main/java/com/pGallery/pGalleryqueryService/PGalleryQueryServiceApplication.java
+++ src/main/java/com/pGallery/pGalleryqueryService/PGalleryQueryServiceApplication.java
@@ -2,11 +2,18 @@ package com.pGallery.pGalleryqueryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PGalleryQueryServiceApplication {
+public class PGalleryQueryServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PGalleryQueryServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(PGalleryQueryServiceApplication.class);
 	}
 }
